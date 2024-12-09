be47f3815e732eb5812b9684daf6e279c9bd5ead
Asad Aslam <opworrier@gmail.com>
2024-12-09 14:00:57 +0500

    first commit

---
+++ login-auth/src/Components/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClS2kJ9sP0Hh-mwg28tiGMp4wZ26wS8tY",
+  authDomain: "login-auth-73c6b.firebaseapp.com",
+  projectId: "login-auth-73c6b",
+  storageBucket: "login-auth-73c6b.firebasestorage.app",
+  messagingSenderId: "463873085598",
+  appId: "1:463873085598:web:c64d36e426ab723f00ab8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
