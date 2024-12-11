564ffe1f04c9898e2059e8026890e228c3c56d67
Asad Aslam <opworrier@gmail.com>
2024-12-11 14:06:34 +0500

    Changes

--- login-auth/src/Login.jsx
+++ login-auth/src/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div>
-      Login
+      <Link to="/SignUp">Click Here To SignUp</Link> 
     </div>
   )
 }
