2dbbc8a9e7246563fbbe7702b93e3ad45224a4bd
Asad Aslam <opworrier@gmail.com>
2025-01-01 12:43:44 +0500

    updates

--- login-auth/src/SignUp.jsx
+++ login-auth/src/SignUp.jsx
@@ -16,7 +16,7 @@ const SignUp = () => {
             createUserWithEmailAndPassword(auth, email,password)
             Swal.fire({
                 title: "Congratulation",
-                text: "Your Account Has Been Created Succesfully",
+                text: "Your Account Has  Created Succesfully",
                 icon: "success"
               });
         }catch(err){
